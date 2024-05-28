9e9eba8cb3260bc8ab4d7de68d457592b412d5b5
jeizadi <jeizadi@tiogamed.com>
2024-05-28 11:42:33 -0700

    Modified program to output area, perimeter, enclosed min circ dia, enclosed rotated rect wxh in MM

--- particle-detection-main.py
+++ particle-detection-main.py
@@ -11,22 +11,28 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+
+import pandas as pd
 import seaborn as sns
 from scipy.spatial.distance import euclidean
 from scipy.stats import norm
 from tkinter import filedialog
 
-global image_path # Path to the image
-global image_name # Reference to the file name of the selected video file
-global original_image # Reference to the OG image file
-global image # Reference to the opened image file
-global image_roi # Reference to the user-defined ROI for contour selection
+global image_path  # Path to the image
+global image_name  # Reference to the file name of the selected video file
+global original_image  # Reference to the OG image file
+global image  # Reference to the opened image file
+global image_roi  # Reference to the user-defined ROI for contour selection
+global points  # Reference to the user-selected points
+global data  # Reference to the dataframe
+
 
 # Open a video file from a path and store the instance
 def open_image_file():
     global image_path, image_name, image, original_image
     # Open a file dialog to select the video file
-    image_file = filedialog.askopenfilename(title="Select an image", filetypes=[("JPEG files", "*.jpg"),("PNG files", "*.png")])
+    image_file = filedialog.askopenfilename(title="Select an image", filetypes=[("JPEG files", "*.jpg"),
+                                                                                ("PNG files", "*.png")])
 
     if not image_file:
         return  # No file selected
@@ -41,7 +47,8 @@ def open_image_file():
     original_height, original_width = image.shape[:2]
     if original_width > 800 or original_height > 600:
         resize_image(800, 600)
-        
+
+
 # Resize an image to fit within w = 800 & h = 600 
 def resize_image(max_width, max_height):
     global image
@@ -62,8 +69,9 @@ def resize_image(max_width, max_height):
 
     # Resize the image
     image = cv2.resize(image, (new_width, new_height))
-    
-def select_point(event,x,y,flags,param):
+
+
+def select_point(event, x, y, flags, param):
     global image, points
     
     # Record starting (x,y) coordinates on left mouse button click
@@ -77,6 +85,7 @@ def select_point(event,x,y,flags,param):
         
         cv2.line(image, points[0], points[1], (36,255,12), 2)
         cv2.imshow("image", image)
+
 
 def preprocess_image():
     global image, image_roi
@@ -94,8 +103,8 @@ def preprocess_image():
     hsv = cv2.cvtColor(image_roi, cv2.COLOR_BGR2HSV)
     
     # Threshold for red color
-    lower = np.array([155,25,0])
-    upper = np.array([179,255,255])
+    lower = np.array([155, 25, 0])
+    upper = np.array([179, 255, 255])
     mask_red = cv2.inRange(hsv, lower, upper)
     
     result = cv2.bitwise_and(result, result, mask=mask_red)
@@ -110,6 +119,7 @@ def preprocess_image():
     
     return closing
 
+
 def count_and_measure_area(binary_image, calibration_factor):
     global image_roi
     # Find contours in the binary image
@@ -119,22 +129,53 @@ def count_and_measure_area(binary_image, calibration_factor):
     contour_overlay = image_roi.copy()
     cv2.drawContours(contour_overlay, contours, -1, (0, 255, 0), 2)  # -1 means draw all contours
     cv2.imshow("contours", contour_overlay)
+
     # Initialize variables to count particles and total area
     particle_count = 0
-    areas = []
-    
+    areas = []  # Approximation contour identification
+    perimeters = []  # Approximation contour identification
+    widths = []  # Rotated rectangle width
+    heights = []  # Rotated rectangle height
+    diameters = []  # Diameter of bounded circle fit
+
+    circle_overlay = image_roi.copy()
+    rectangle_overlay = image_roi.copy()
+
     # Iterate through each contour
     for contour in contours:
-        # Calculate the area of the contour
+        # Calculate the area and perimeter of the contour
         area = cv2.contourArea(contour)
-        
-        area = area*calibration_factor
+        perimeter = cv2.arcLength(contour, True)
+
+        # Draw a rectangle around the perimeter of the contour
+        (x, y), (width, height), angle = cv2.minAreaRect(contour)
+        box = cv2.boxPoints(((x, y), (width, height), angle))
+        box = np.int0(box)
+        cv2.drawContours(rectangle_overlay, [box], 0, (0, 0, 255), 2)
+
+        # Minimum enclosing circle approximation
+        (x, y), radius = cv2.minEnclosingCircle(contour)
+        diameter = 2 * int(radius)
+        center = (int(x), int(y))
+        radius = int(radius)
+        cv2.circle(circle_overlay, center, radius, (0, 255, 0), 2)
+
         if area > 0:
             particle_count += 1
-            areas.append('%.3f'%(area))
-                
-    return particle_count, areas, contour_overlay
+            areas.append('%.3f' % (area * calibration_factor * calibration_factor))
+            perimeters.append('%.3f' % (perimeter * calibration_factor))
+            diameters.append('%.3f' % (diameter * calibration_factor))
+            widths.append('%.3f' % (width * calibration_factor))
+            heights.append('%.3f' % (height * calibration_factor))
+        cv2.imshow("circles", circle_overlay)
+        cv2.imshow("rectangles", rectangle_overlay)
 
+    # data = pd.DataFrame(areas, perimeters, diameters, widths, heights)  # Add data to a dataframe for storage
+    return (particle_count, areas, perimeters, diameters, widths, heights, contour_overlay, circle_overlay,
+            rectangle_overlay)
+
+
+'''
 def process_data(Actual_unit, unit, areas):
     if Actual_unit != unit:
         if Actual_unit == 'mm': # Need to convert to inch
@@ -144,39 +185,35 @@ def process_data(Actual_unit, unit, areas):
     else:
         areas = [float(area) for area in areas]
     return areas
-        
-def create_histogram(data, x_scale, bin_size, units, file):
+'''
+
+
+def create_histogram(data, x_scale, bin_size, file):
     bins = np.arange(0, x_scale + bin_size, bin_size)
-    plt.hist(data, bins=bins, edgecolor='black') # Plot the histogram
-    plt.xlabel(f'Area ({units}\u00b2)')
+    plt.hist(data, bins=bins, edgecolor='black')  # Plot the histogram
+    plt.xlabel(f'Area (mm)')
     plt.ylabel('Frequency')
     plt.title('Coating Particulate: {:.2f} to {:.2f}'.format(0, x_scale))
     plt.grid(True)
-    plt.savefig(file) # Save the histogram
-    plt.show() # Display the plot
+    plt.savefig(file)
+    plt.show()
 
-def create_graphic(areas, x_scale, unit, file):
+
+def create_graphic(areas, x_scale, file):
     mu, std = norm.fit(areas)
     fig, ax = plt.subplots()
     sns.histplot(data=areas, binwidth=x_scale/10, ax=ax, kde=True)
     ax.set_xlim(0, x_scale)
-    plt.xlabel(f'Area ({unit}\u00b2)')
+    plt.xlabel(f'Area (mm)')
     plt.title('Coating Particulate: {:.2f} and {:.2f}'.format(mu, std))
-    plt.savefig(file) # Save the graphic
+    plt.savefig(file)  # Save the graphic
     plt.show()
-    
-def output(particle_count, areas, contour_overlay, unit): 
-    # Print the total area and max area
-    total_area = sum(areas)
-    max_area = max(areas)
-    
-    # Print the results
-    print(f"Number of Red Particles: {particle_count}")
-    print(f"Total Area: {total_area:.3f}")
-    print(f"Max Area: {max_area:.3f}")
-    
-    foldername = image_name.split('.')[0] 
-    filepath = os.path.join(image_path, foldername)
+
+
+def output(particle_count, areas, perimeters, diameters, widths, heights, contour_overlay, circle_overlay,
+           rectangle_overlay):
+    folder_name = image_name.split('.')[0]
+    filepath = os.path.join(image_path, folder_name)
     
     # Create the file path if one does not exist already
     if not os.path.exists(filepath):
@@ -194,31 +231,41 @@ def output(particle_count, areas, contour_overlay, unit):
     file_extension = filename + '.csv'
     path = os.path.join(filepath, file_extension)
     with open(path, 'w', newline='') as file:
-        file.write('Areas Given In ' + unit.upper() + ',Area-Derived Diameter\n')  # Write the header
-        for area in areas:
-            diameter = 2 * math.sqrt(area / math.pi)
-            file.write(str(area) + ',' + str(diameter) + '\n')  # Write data for both columns
-    
+        file.write('Area(mm), Perimeter(mm), Diameter(mm), Width(mm), Height(mm)\n')  # Write the header
+        for area, perimeter, diameter, width, height in zip(areas, perimeters, diameters, widths, heights):
+            file.write(str(area) + ',' + str(perimeter) + ',' + str(diameter) + ',' + str(width) + ',' + str(height) +
+                       '\n')  # Write data for both columns
+
+    '''
     # Save distribution graphics for specific size bins
-    file_extension = filename + '_0_to_50' + unit + '.png'
+    file_extension = filename + '_0_to_50mm.png'
     path = os.path.join(filepath, file_extension)
-    create_graphic(areas, 50, unit, path)
+    create_graphic(areas, 50, path)
     
-    file_extension = filename + '_0_to_5' + unit + '.png'
+    file_extension = filename + '_0_to_5mm.png'
     path = os.path.join(filepath, file_extension)
-    create_graphic(areas, 5, unit, path)
+    create_graphic(areas, 5, path)
     
-    file_extension = filename + '_0_to_1' + unit + '.png'
+    file_extension = filename + '_0_to_1mm.png'
     path = os.path.join(filepath, file_extension)
-    create_graphic(areas, 1, unit, path)
-    
+    create_graphic(areas, 1, path)
+    '''
+
     # Save the image with the contours drawn 
-    file_extension = filename + '_contours.png'
-    
+    filename = filename + '_contours.png'
+    os.chdir(filepath)
+    cv2.imwrite(filename, contour_overlay)
+    filename = filename + '_circles.png'
+    os.chdir(filepath)
+    cv2.imwrite(filename, circle_overlay)
+    filename = filename + '_rectangles.png'
+    os.chdir(filepath)
+    cv2.imwrite(filename, rectangle_overlay)
     return
-    
+
+
 def main(): 
-    open_image_file() # Open the image file
+    open_image_file()  # Open the image file
  
     global points
     points = []
@@ -229,25 +276,28 @@ def main():
     cv2.imshow('image',image)
     cv2.waitKey(0)
     
-    px= euclidean(points[0], points[1])
-    Actual_dimensions= float(input("Enter dimensions: "))
-    Actual_unit = input("Enter your unit(mm, inch): ")
-    
-    calibration_factor= Actual_dimensions/px
-    calibration_factor= calibration_factor**2
-    
+    px = euclidean(points[0], points[1])
+    cal_dim = float(input("Enter dimensions: "))
+    #Actual_unit = input("Enter your unit(mm, inch): ")
+    calibration_factor = cal_dim / px
+
     cv2.destroyAllWindows()
     
     # Preprocess the image
     binary_image = preprocess_image()
     
     # Count and measure the area of red particles
-    particle_count, areas, contour_overlay = count_and_measure_area(binary_image, calibration_factor)
-    
+    particle_count, areas, perimeters, diameters, widths, heights, contour_overlay, circle_overlay, rectangle_overlay\
+        = count_and_measure_area(binary_image, calibration_factor)
+
+    '''
     unit = input("Enter your output unit(mm, inch): ")
-    
     areas = process_data(Actual_unit, unit, areas) # Process the data for applicable units
-    output(particle_count, areas, contour_overlay, unit) # Output max area, total area, particle count. Save to file.     
+    output(particle_count, areas, contour_overlay, unit)  # Output max area, total area, particle count. Save to file.  
+    '''
+
+    output(particle_count, areas, perimeters, diameters, widths, heights, contour_overlay, circle_overlay,
+           rectangle_overlay)
         
     # De-allocate any associated memory usage   
     if cv2.waitKey(0) & 0xff == 27:  
